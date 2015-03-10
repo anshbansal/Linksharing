05f7db8da697035dcf3fea0afabb7653a8c009f2
Aseem Bansal <aseemb@intelligrape.com>
2015-03-10 15:49:17 +0530

    Removed unused code

--- grails-app/domain/com/ttd/linksharing/domain/ReadingItem.groovy
+++ grails-app/domain/com/ttd/linksharing/domain/ReadingItem.groovy
@@ -8,17 +8,6 @@ class ReadingItem {
     static belongsTo = [resource: Resource, user: User]
 
     static namedQueries = {
-
-        isReadForUser { Resource resource, User user ->
-            projections {
-                property('isRead')
-            }
-            eq 'user', user
-            eq 'resource', resource
-
-            fetchMode('user', FetchMode.JOIN)
-        }
-
         unreadForUser { User user ->
             eq ('user', user)
             eq('isRead', Boolean.FALSE)
