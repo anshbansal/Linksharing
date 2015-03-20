8bd6ee5947dbc3acc15210cdcd1c174e42ae9f38
Aseem Bansal <aseemb@intelligrape.com>
2015-03-20 17:42:50 +0530

    FIxed mistake of putting the absolute path inside database

--- grails-app/controllers/com/ttd/linksharing/controller/domain/DocumentResourceController.groovy
+++ grails-app/controllers/com/ttd/linksharing/controller/domain/DocumentResourceController.groovy
@@ -14,31 +14,23 @@ class DocumentResourceController {
     def create(DocumentResourceCO documentResourceCO) {
         if (!subscriptionService.isUserSubscribedToTopic(session?.loggedUser, documentResourceCO.documentTopic)) {
             render "You are not authorized to create link for this topic"
+            return
         }
 
-        String documentRoot = grailsApplication.config.grails.linksharing.files.rootPath
-        String filePath = "${documentRoot}/${session?.loggedUser?.id}/${documentResourceCO.documentTopic?.id}"
-        String fileNameWithPath = "${filePath}/${documentResourceCO.document.originalFilename}"
+        String filePathWithoutDocumentRoot = "/${session?.loggedUser?.id}/${documentResourceCO.documentTopic?.id}/${documentResourceCO.document.originalFilename}"
 
         if (documentResourceCO.hasErrors()) {
             render "Details had errors ${documentResourceCO.errors}"
-        } else if (documentResourceService.create(documentResourceCO, session?.loggedUser, fileNameWithPath)) {
-            new File(filePath).mkdirs()
-            documentResourceCO.document.transferTo(new File(fileNameWithPath))
+        } else if (documentResourceService.create(documentResourceCO, session?.loggedUser, filePathWithoutDocumentRoot)) {
+
+            String documentRoot = grailsApplication.config.grails.linksharing.files.rootPath
+            String filePathWithDocumentRoot = "${documentRoot}/${filePathWithoutDocumentRoot}"
+
+            new File(filePathWithDocumentRoot).mkdirs()
+            documentResourceCO.document.transferTo(new File(filePathWithDocumentRoot))
             render "Document Created"
         } else {
             render "Error occurred"
         }
     }
-
-//    def saveDocument(DocumentResourceCO documentResourceCO){
-//        User user=User.findByUsername((String)session["username"])
-//        MultipartFile file=documentResourceCO.file
-//        String path=request.getRealPath('/')
-//        path=path+'/rohan/'+file.originalFilename
-//// new File(path).createNewFile()
-//        file.transferTo(new File(path))
-//        documentResourceService.saveDocumentResource(user,documentResourceCO,path)
-//        redirect(controller: "home",action: "index")
-//    }
 }
